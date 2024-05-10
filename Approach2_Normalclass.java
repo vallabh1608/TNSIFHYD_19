c6cba04832622c6eee8b5229c68db6a8c2c4b8b9
vallabh1608 <158051290+vallabh1608@users.noreply.github.com>
2024-05-10 17:17:32 +0530

    Add files via upload

---
+++ Approach2_Normalclass.java
@@ -0,0 +1,13 @@
+package myjava1;
+
+public class Approach2_Normalclass {
+	String batsman="ViratKohli";
+	static String bowler="Mohd shami"; 
+	void display() {
+			System.out.println("Cricket is outdoor game");
+	}
+	static String display1() {
+		return "Cricket first played in England";
+	}
+
+}
---
+++ Typecast.java
@@ -0,0 +1,17 @@
+package myjava1;
+
+public class Typecast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		short b=(short)a;
+		float c=2.5f;
+		double d=c;
+		System.out.println(b);
+		System.out.println(d);
+
+
+	}
+
+}
